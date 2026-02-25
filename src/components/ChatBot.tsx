bf56d730d41260d3d91d3d90ad1a69c7d157a4c4
Lalomax <lalomax365@gmail.com>
2026-02-25 08:01:09 -0400

    perf chat button overlapping scroll to top button

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -88,7 +88,7 @@ const ChatBot: React.FC = () => {
   };
 
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed bottom-8 right-4 z-50">
       {/* Chat Button */}
       {!isOpen && (
         <motion.button
